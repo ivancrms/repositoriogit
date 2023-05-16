fea723a00185376448db5271a43f014e9a23d291
ivancrms <115224656+ivancrms@users.noreply.github.com>
2023-05-16 14:11:39 +0200

    Delete deploy-stack.bat

--- deploy-stack.bat
+++
@@ -1,9 +0,0 @@
-set %Instance-Name="Ubuntu"
-set %Accountid=094375575646
-aws cloudformation deploy --stack-name UbuntuServer20 --template-file main.yml --parameter-overrides "Instance-Name=%Instance-name%"
-
-if %ERRORLEVEL% neq 0 (
-   echo Se han producido errores al crear la pila
-) else (
-    aws cloudformation list-exports --query "Exports[?Name=='ServerPublicIp'].Value"
-)
